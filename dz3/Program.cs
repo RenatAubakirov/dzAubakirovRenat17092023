286b92e4e4d51f1d905e43bacd6c279d2a604e60
Renat Aubakirov <arenat98@mail.ru>
2023-09-21 16:06:30 +0700

    dz6

--- dz3/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int a, b, c;
-a = Convert.ToInt32(Console.ReadLine());
-b = Convert.ToInt32(Console.ReadLine());
-c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (max<b) max = b;
-if (max<c) max = c;
-System.Console.WriteLine(max);
